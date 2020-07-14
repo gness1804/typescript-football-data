a284667f68a0f594728ddff0f5345cc18108c379
gness1804 <graham.nessler@gmail.com>
2020-07-13 21:34:13 -0500

    Set up reading of csv data

--- build/index.js
+++ build/index.js
@@ -1,4 +1,9 @@
 "use strict";
-const myVar = 'foo';
-console.log('myVar', myVar);
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = require("fs");
+const matches = fs_1.readFileSync('football.csv', {
+    encoding: "utf-8",
+})
+    .split('\n')
+    .map((entry) => entry.split(','));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-const myVar = 'foo';
+import {readFileSync} from 'fs';
 
-console.log('myVar', myVar);
+const matches: string[][] = readFileSync('football.csv', {
+    encoding: "utf-8",
+})
+    .split('\n')
+    .map((entry: string): string[] =>
+    entry.split(','))
