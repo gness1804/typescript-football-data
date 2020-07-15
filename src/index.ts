e428943f496c9947bbbc0ebc6b77998c63be5915
gness1804 <graham.nessler@gmail.com>
2020-07-14 21:23:35 -0500

    Add prettier

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
-import {readFileSync} from 'fs';
+import { readFileSync } from 'fs';
 
 const matches: string[][] = readFileSync('football.csv', {
-    encoding: "utf-8",
+  encoding: 'utf-8',
 })
-    .split('\n')
-    .map((entry: string): string[] =>
-        entry.split(','))
+  .split('\n')
+  .map((entry: string): string[] => entry.split(','));
 
 let count = 0;
 
 for (const match of matches) {
-    if ((match[1] === 'Man United' && match[5] === 'H') || (match[2] === 'Man United' && match[5] === 'A')) count++;
+  if (
+    (match[1] === 'Man United' && match[5] === 'H') ||
+    (match[2] === 'Man United' && match[5] === 'A')
+  )
+    count++;
 }
 
 /* eslint-disable-next-line no-console */
